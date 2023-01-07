e7e45c04f38524ff1a8b7a4c97fe42c84fe2586e
Cooper <2597905281@qq.com>
2023-01-07 16:17:34 +0800

    feat: add toggle button component

---
+++ src/stories/ToggleButton.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { ToggleButton } from '../components/ToggleButton'
+
+
+storiesOf('Toggle', module)
+  .add('button', () => <ToggleButton/>);
+
